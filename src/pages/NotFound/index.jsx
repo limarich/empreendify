1a07e8a78e0f86e51c999c6bd0872d013d07f87b
Richard <richard.esclima@gmail.com>
2023-03-19 12:11:38 -0300

    feat: not found page

---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,3 @@
+export const NotFound = () => {
+  return <div>404 página não encontrada</div>;
+};
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes as Switch } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
+import { NotFound } from "../pages/NotFound";
 
 export const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" Component={Login} />
       <Route path="/home" Component={Home} />
+      <Route path="*" Component={NotFound} />
     </Switch>
   );
 };
